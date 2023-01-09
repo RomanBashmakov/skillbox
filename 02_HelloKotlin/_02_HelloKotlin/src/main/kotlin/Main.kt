6e8db647e90d216e3731882797c2776fe5c5af7b
RomanBashmakov <bashmakov_r@inbox.ru>
2023-01-09 19:42:51 +0400

    02_HelloKotlin by Bashmakov Roman

---
+++ 02_HelloKotlin/_02_HelloKotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+
+    //Task #1
+    println("Hello Tutor!")
+
+    print("My name is ")
+    println("Roman")
+    println("I go by Roma")
+
+    //Task #2
+
+    /*
+    This is the first homework
+    */
+
+    //Task #3
+    //print ("This line is commented out")
+}
