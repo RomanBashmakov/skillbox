c3162518ec8b1831025c400409423c8542ee7ee0
RomanBashmakov <bashmakov_r@inbox.ru>
2023-01-15 21:28:14 +0400

    Lesson #3

--- 03_Variables_types/_03_Variables_types/src/main/kotlin/Main.kt
+++ 03_Variables_types/_03_Variables_types/src/main/kotlin/Main.kt
@@ -1,7 +1,34 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    //The tasks from #1 to #5
+    val firstName: String = "Roman"
+    val lastName: String = "Bashmakov"
+    var height: Double = 1.72
+    var weight: Float = 86.0F
+
+    //Auxiliary function
+    var info: String = myAppearance(firstName, lastName, height, weight)
+    println("The first variant:\n" + info)
+
+    //New height
+    height = 1.4
+    println("New height = $height m\n")
+
+    //Taking into account the new value
+    info = myAppearance(firstName, lastName, height, weight)
+    println("The second variant:\n" + info)
+}
+
+fun myAppearance(_firstName: String,
+                 _lastName: String,
+                 _height: Double,
+                 _weight: Float): String {
+    val _isChild: Boolean = _height < 1.5 || _weight < 40
+    val myAppearance: String =
+        "My first name: $_firstName\n" +
+        "My last name: $_lastName\n" +
+        "My height: $_height m\n" +
+        "My weight: $_weight kg\n" +
+        "I am a child? - $_isChild\n"
+    return myAppearance
 }
